a104e427ebae5654513536e0892cbfef3cc7a89a
Maldace <phucphuong2112@gmail.com>
2025-09-14 15:30:19 +0700

    Thêm trang profile

---
+++ app/src/main/java/com/example/computerselling/ProfileActivity.java
@@ -0,0 +1,6 @@
+package com.example.computerselling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/computerselling/RegisterActivity.java
+++ app/src/main/java/com/example/computerselling/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.computerselling;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spnGender);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Nam", "Nữ"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 
     public  void loginPageRedirect(View view){
