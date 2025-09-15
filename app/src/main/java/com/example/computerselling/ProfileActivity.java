fb18752aa619c7224e92310c3e412c6ee7542c0e
Maldace <phucphuong2112@gmail.com>
2025-09-15 22:12:58 +0700

    Thiết kế trang profile

--- app/src/main/java/com/example/computerselling/ProfileActivity.java
+++ app/src/main/java/com/example/computerselling/ProfileActivity.java
@@ -1,6 +1,21 @@
 package com.example.computerselling;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+    public void homePage(View view){
+        Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
